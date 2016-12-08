0ad2d4a9de2b938ddba6d0e4d95daf8a2d5e0336
Imran Merchant <imerchant@outlook.com>
2016-12-08 16:23:52 -0600

    added test project and helpers

---
+++ tests/IM.AdventOfCode2016.Tests/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Xunit.Abstractions;
+
+namespace IM.AdventOfCode2016.Tests
+{
+	public abstract class TestBase
+	{
+		protected ITestOutputHelper Output { get; }
+
+		protected TestBase(ITestOutputHelper output)
+		{
+			Output = output;
+		}
+	}
+
+	public static class TestExtensions
+	{
+		private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+		public static void WriteObject<T>(this ITestOutputHelper output, T value) where T : class
+		{
+			var json = JsonConvert.SerializeObject(value, Formatting.Indented, Settings);
+		}
+	}
+}
