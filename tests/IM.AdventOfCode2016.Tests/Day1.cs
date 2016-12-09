9b6e8e57b9f04e86661adba9df9978d227b0bde1
Imran Merchant <imerchant@outlook.com>
2016-12-09 13:47:29 -0600

    added Inputs.cs and Day2 inputs

--- tests/IM.AdventOfCode2016.Tests/Day1.cs
+++ tests/IM.AdventOfCode2016.Tests/Day1.cs
@@ -8,7 +8,7 @@ namespace IM.AdventOfCode2016.Tests
 {
 	public class Day1 : TestBase
 	{
-		private const string RawInstructions = @"R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2";
+		private const string RawInstructions = Inputs.Day1;
 		private readonly State _startingState;
 
 		public Day1(ITestOutputHelper output) : base(output)
